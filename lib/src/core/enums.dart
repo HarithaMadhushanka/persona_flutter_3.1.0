8ab15abccf935cf3cd689b2788d5e49fb56f8d7b
Haritha Rajarathne <haritha.rajarathne@hitachi-dps.com>
2023-11-20 16:55:55 +0800

    Initial commit

---
+++ lib/src/core/enums.dart
@@ -0,0 +1,20 @@
+/// The Persona API environment on which to create inquiries.
+enum InquiryEnvironment {
+  /// The development environment.
+  sandbox,
+
+  /// The production environment.
+  production
+
+  /// The production - staging environment.
+  env_YWAFSMCNEoGUB2MYXCh5SAz8
+}
+
+/// Source for theme value
+enum InquiryThemeSource {
+  /// Instructs the SDK to use theme values set in the Persona Dashboard
+  server,
+
+  /// Uses theme values set on client
+  client,
+}
